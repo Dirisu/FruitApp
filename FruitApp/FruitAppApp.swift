e7cfa927440a3edd18f66ca9a1d490a400ba1ad9
Dirisu <dirisumarvellous94@gmail.com>
2023-06-18 07:05:54 +0100

    Initial Commit

---
+++ FruitApp/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  FruitApp
+//
+//  Created by Marvellous Dirisu on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
